0c7049bae9fc138e9521a21e332b74b8d8e9c751
Jarek <jaroslaw.wronka@luxmed.pl>
2024-01-15 12:48:52 +0100

    Initial Commit

---
+++ Data1/NewContactViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NewContactViewController.swift
+//  Data1
+//
+//  Created by Boss on 15/01/2024.
+//
+
+import UIKit
+
+class NewContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
---
+++ Data1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Data1
+//
+//  Created by Boss on 15/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
