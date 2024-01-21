4439393b74af912ef632d86f980cffb25a3dba25
Jarek <jaroslaw.wronka@luxmed.pl>
2024-01-21 11:42:46 +0100

    Update

--- Data1/ViewController.swift
+++ Data1/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    struct user {
+        var login : String
+        var password : String
+        
+    }
+    
+    var users : [user] = []
+
+    @IBOutlet weak var Login: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UILoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if ((Login.text == users[0].login ) && (Password.text == users[0].password)) {
+            return true
+        } else { return false}
+    }
+    
+    func UILoad(){
+        
+        users.append(.init(login: "jarek", password: "haslo"))
+        //users[0].login = "jarek"
+        //users[0].password = "haslo"
+        
+    }
 
-
+    @IBAction func VerifyLogin(_ sender: UIButton) {
+        
+        
+    }
+    
 }
 
